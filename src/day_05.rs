044fde79ac7b9d0b91094b4dac59f77a9f6f7e44
Siraaj Khandkar <siraaj@khandkar.net>
2022-12-06 01:23:37 -0500

    Clean-up day 05

--- src/day_05.rs
+++ src/day_05.rs
@@ -5,12 +5,16 @@ use anyhow::{anyhow, Result};
 
 #[derive(Debug, Clone)]
 enum Ix {
-    Mv { quant: usize, src: u64, dst: u64 },
+    Mv {
+        quant: usize,
+        src: usize,
+        dst: usize,
+    },
 }
 
 #[derive(Debug, Clone)]
 pub struct Data {
-    stacks: HashMap<u64, Vec<char>>,
+    stacks: Vec<Vec<char>>,
     instructions: Vec<Ix>,
 }
 
@@ -24,7 +28,7 @@ impl Data {
     pub fn load(input: &std::path::Path) -> Result<Self> {
         let file = std::fs::File::open(input)?;
         let mut section = Section::Cts;
-        let mut stacks: HashMap<u64, Vec<char>> = HashMap::new();
+        let mut stacks: HashMap<usize, Vec<char>> = HashMap::new();
         let mut instructions: Vec<Ix> = Vec::new();
         for line_result in std::io::BufReader::new(file).lines() {
             let line = line_result?;
@@ -43,7 +47,7 @@ impl Data {
                                         let mut stack =
                                             match stacks.get(&stack_id) {
                                                 None => Vec::new(),
-                                                Some(s) => s.to_vec(),
+                                                Some(stack) => stack.to_vec(),
                                             };
                                         stack.push(crate_name);
                                         stacks.insert(stack_id, stack);
@@ -67,8 +71,8 @@ impl Data {
                     match line.split_whitespace().collect::<Vec<&str>>()[..] {
                         ["move", quant, "from", src, "to", dst] => {
                             let quant: usize = quant.parse()?;
-                            let src: u64 = src.parse()?;
-                            let dst: u64 = dst.parse()?;
+                            let src: usize = src.parse()?;
+                            let dst: usize = dst.parse()?;
                             instructions.push(Ix::Mv { quant, src, dst });
                         }
                         _ => {
@@ -80,58 +84,45 @@ impl Data {
                 }
             }
         }
-        for (_, stack) in stacks.iter_mut() {
+        let mut stacks_vec = vec![Vec::new(); stacks.len() + 1];
+        for (i, stack) in stacks.iter_mut() {
             stack.reverse();
+            stacks_vec[*i] = stack.to_vec();
         }
         Ok(Self {
-            stacks,
+            stacks: stacks_vec,
             instructions,
         })
     }
 
     pub fn part1(&self) -> Result<String> {
-        let mut data = self.clone();
+        let mut stacks = self.stacks.clone();
         for Ix::Mv { quant, src, dst } in self.instructions.iter() {
             for _ in 0..*quant {
-                let src_stack = data
-                    .stacks
-                    .get_mut(src)
-                    .unwrap_or_else(|| unreachable!());
-                let krate = src_stack.pop().unwrap_or_else(|| unreachable!());
-                let dst_stack = data
-                    .stacks
-                    .get_mut(dst)
-                    .unwrap_or_else(|| unreachable!());
-                dst_stack.push(krate);
+                let krate =
+                    stacks[*src].pop().unwrap_or_else(|| unreachable!());
+                stacks[*dst].push(krate);
             }
         }
-        let max = *data.stacks.keys().max().unwrap_or_else(|| unreachable!());
-        let min = *data.stacks.keys().min().unwrap_or_else(|| unreachable!());
-        let mut msg = String::new();
-        for i in min..max + 1 {
-            let stack = data.stacks.get(&i).unwrap_or_else(|| unreachable!());
-            stack.last().iter().for_each(|c| msg.push(**c))
-        }
-        Ok(msg)
+        Ok(Self::msg(stacks))
     }
 
     pub fn part2(&self) -> Result<String> {
-        let mut data = self.clone();
+        let mut stacks = self.stacks.clone();
         for Ix::Mv { quant, src, dst } in self.instructions.iter() {
-            let src_stack =
-                data.stacks.get_mut(src).unwrap_or_else(|| unreachable!());
-            let mut krates = src_stack.split_off(src_stack.len() - quant);
-            let dst_stack =
-                data.stacks.get_mut(dst).unwrap_or_else(|| unreachable!());
-            dst_stack.append(&mut krates);
+            let len = stacks[*src].len();
+            let mut krates = stacks[*src].split_off(len - quant);
+            stacks[*dst].append(&mut krates);
         }
-        let max = *data.stacks.keys().max().unwrap_or_else(|| unreachable!());
-        let min = *data.stacks.keys().min().unwrap_or_else(|| unreachable!());
-        let mut msg = String::new();
-        for i in min..max + 1 {
-            let stack = data.stacks.get(&i).unwrap_or_else(|| unreachable!());
-            stack.last().iter().for_each(|c| msg.push(**c))
-        }
-        Ok(msg)
+        Ok(Self::msg(stacks))
+    }
+
+    fn msg(stacks: Vec<Vec<char>>) -> String {
+        stacks
+            .iter()
+            .map(|stack| stack.last().unwrap_or(&' '))
+            .collect::<String>()
+            .trim()
+            .to_string()
     }
 }
