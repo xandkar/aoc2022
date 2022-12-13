56e5a5dc2adb8e34554d6fd78ffc904b1c9ab22f
Siraaj Khandkar <siraaj@khandkar.net>
2022-12-13 17:48:48 -0500

    Org tests into submodules

--- tests/tests.rs
+++ tests/tests.rs
@@ -25,29 +25,35 @@ macro_rules! path_to_input {
 macro_rules! day {
   ($n:ident, $p1_ex:expr, $p1_in:expr, $p2_ex:expr, $p2_in:expr) => {
        paste::paste! {
-            #[test]
-            fn [<day $n _part_1 _example>]() {
-                let data = aoc2022::[<day $n>]::Data::load(path_to_example!($n)).unwrap();
-                assert_eq!($p1_ex, data.solve1().unwrap());
-            }
+           #[cfg(test)]
+           mod [<day $n>] {
+               mod part_1 {
+                   #[test]
+                   fn example() {
+                       let data = aoc2022::[<day $n>]::Data::load(path_to_example!($n)).unwrap();
+                       assert_eq!($p1_ex, data.solve1().unwrap());
+                   }
 
-            #[test]
-            fn [<day $n _part_1 _input>]() {
-                let data = aoc2022::[<day $n>]::Data::load(path_to_input!($n)).unwrap();
-                assert_eq!($p1_in, data.solve1().unwrap());
-            }
+                   #[test]
+                   fn input() {
+                       let data = aoc2022::[<day $n>]::Data::load(path_to_input!($n)).unwrap();
+                       assert_eq!($p1_in, data.solve1().unwrap());
+                   }
+               }
+               mod part_2 {
+                    #[test]
+                    fn example() {
+                        let data = aoc2022::[<day $n>]::Data::load(path_to_example!($n)).unwrap();
+                        assert_eq!($p2_ex, data.solve2().unwrap());
+                    }
 
-            #[test]
-            fn [<day $n _part_2 _example>]() {
-                let data = aoc2022::[<day $n>]::Data::load(path_to_example!($n)).unwrap();
-                assert_eq!($p2_ex, data.solve2().unwrap());
-            }
-
-            #[test]
-            fn [<day $n _part_2 _input>]() {
-                let data = aoc2022::[<day $n>]::Data::load(path_to_input!($n)).unwrap();
-                assert_eq!($p2_in, data.solve2().unwrap());
-            }
+                    #[test]
+                    fn input() {
+                        let data = aoc2022::[<day $n>]::Data::load(path_to_input!($n)).unwrap();
+                        assert_eq!($p2_in, data.solve2().unwrap());
+                    }
+               }
+           }
        }
   };
 }
